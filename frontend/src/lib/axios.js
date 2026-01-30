5c26a4f052fe3fc90b9abc47c218b4d36e2b8ee6
harishkaushik234 <harishkaushik2034@gmail.com>
2026-01-30 15:19:00 +0530

    Fix axios baseURL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,19 @@
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+// export const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true, // send cookies with the request
+// });
+
+
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+// ✅ Render + Vercel compatible base URL
+const BASE_URL = import.meta.env.VITE_API_URL + "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+  withCredentials: true, // send cookies with request
 });
